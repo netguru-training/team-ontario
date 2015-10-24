0a01b343d2522cf562a48dbc92907ada278fada5
Juliusz Nadberezny <nadberezny@gmail.com>
2015-10-24 12:27:32 +0200

    Add Comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,3 @@
+class Comment < ActiveRecord::Base
+  belongs_to :kid_event
+end
--- app/models/kid_event.rb
+++ app/models/kid_event.rb
@@ -1,4 +1,5 @@
 class KidEvent < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
+  has_many :comments
 end
