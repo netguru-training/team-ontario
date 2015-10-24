26e09b4ddfbb4683798dabb770b7963ccd5be0bf
Juliusz Nadberezny <nadberezny@gmail.com>
2015-10-24 14:21:28 +0200

    Autheticate user in Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  decent_configuration { strategy DecentExposure::StrongParametersStrategy }
 
   rescue_from CanCan::AccessDenied do |exception|
   flash[:error] = "Access denied."
