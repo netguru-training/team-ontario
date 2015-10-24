30eecbfeb80da0ffa104025015f6e2737d46f28f
Grzegorz Bugala <drunkwolly@gmail.com>
2015-10-24 16:37:54 +0200

    Add strong parameters to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:name,
+        :email, :password, :password_confirmation, :current_password)
+    end
+  end
 
   rescue_from CanCan::AccessDenied do |exception|
   flash[:error] = "Access denied."
   redirect_to root_url
+  end
 end
-end
+
