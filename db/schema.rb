ee63cb8a7db87b741ad2d0c77087ae69cfce1b65
Grzegorz Bugala <drunkwolly@gmail.com>
2015-10-24 10:59:14 +0200

    Add migrations for events model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024084820) do
+ActiveRecord::Schema.define(version: 20151024084902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "points"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "families", force: :cascade do |t|
     t.string   "name"
