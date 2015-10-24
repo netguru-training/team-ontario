8f2f7b81d18ac001a1c113ff16eceb24a2ff70ea
Magda Wiśniewska <magda@bardins.pl>
2015-10-24 17:18:12 +0200

    Add callback to prevent kids access ParentPanel#index

--- app/controllers/parent_panel_controller.rb
+++ app/controllers/parent_panel_controller.rb
@@ -1,4 +1,11 @@
 class ParentPanelController < ApplicationController
   expose(:family_members) { current_user.family.users.select { |user| user != current_user } }
+  expose(:event) { Event.new }
+  expose(:events_types) { EventsType.all }
 
+  before_action :authenticate_user!, :verify_parent_role
+
+  def verify_parent_role
+    redirect_to(root_path, notice: "Next time you'll be punished!") if current_user.has_role?(:kid)
+  end
 end
