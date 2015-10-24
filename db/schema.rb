366ab0216069f13c93b175b6ffb96633e0a388be
Magda Wiśniewska <magda@bardins.pl>
2015-10-24 12:53:14 +0200

    Update schema after adding devise_invitable to users migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024102530) do
+ActiveRecord::Schema.define(version: 20151024104512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(version: 20151024102530) do
     t.integer  "points"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.integer  "events_type_id"
     t.integer  "family_id"
+    t.integer  "events_type_id"
   end
 
   add_index "events", ["events_type_id"], name: "index_events_on_events_type_id", using: :btree
@@ -81,10 +81,21 @@ ActiveRecord::Schema.define(version: 20151024102530) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.integer  "family_id"
+    t.string   "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer  "invitation_limit"
+    t.integer  "invited_by_id"
+    t.string   "invited_by_type"
+    t.integer  "invitations_count",      default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["family_id"], name: "index_users_on_family_id", using: :btree
+  add_index "users", ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
+  add_index "users", ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
+  add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "users_roles", id: false, force: :cascade do |t|
