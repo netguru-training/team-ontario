9dc6194a2febdd5d79bbe52029c32a2affef05ae
Grzegorz Bugala <drunkwolly@gmail.com>
2015-10-24 12:16:26 +0200

    Repair model event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :family
-  belongs_to :event_type
+  belongs_to :events_type
   validates_presence_of :name, :points
   has_many :kid_events
   has_many :users, through: :kid_events
