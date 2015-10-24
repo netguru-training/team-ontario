8723cb71fabebfd563fbf3f91ec267954768d07e
Juliusz Nadberezny <nadberezny@gmail.com>
2015-10-24 14:18:55 +0200

    Add devise authetication to Application controller. Create Events controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,32 @@
+class EventsController < ApplicationController
+  expose (:events, ancestor: :family)
+  expose (:event, ancestor: :family)
+
+  def create
+    if event.save
+      redirect_to event_path(event), notice: 'Event was succesfully created'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if event.save
+      redirect_to event_path(event), notice: 'Event was succesfully created'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    event.destroy
+    redirect_to events_path, notice: 'Event was succesfully deleted'
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :points)
+  end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
+
+  resources :families do
+    resources :events
+  end
 end
