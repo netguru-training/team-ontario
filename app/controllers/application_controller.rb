3343f5276339b210caced7a580b42b5bb9d150c2
Juliusz Nadberezny <nadberezny@gmail.com>
2015-10-24 14:43:22 +0200

    Correct App controller - devise authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  decent_configuration { strategy DecentExposure::StrongParametersStrategy }
 
   rescue_from CanCan::AccessDenied do |exception|
   flash[:error] = "Access denied."
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
-  expose (:events, ancestor: :family)
-  expose (:event, ancestor: :family)
+  expose(:events, ancestor: :family)
+  expose(:event, ancestor: :family)
+  expose(:family)
 
   def create
     if event.save
