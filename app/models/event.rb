f2a59aa13ca12e41908864c7648602460bfe4f46
Magda Wiśniewska <magda@bardins.pl>
2015-10-24 11:41:40 +0200

    Add Family reference to Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,4 @@
 class Event < ActiveRecord::Base
   belongs_to :family
-<<<<<<< HEAD
   belongs_to :event_type
-=======
->>>>>>> Add Family reference to Event
 end
--- app/models/family.rb
+++ app/models/family.rb
@@ -1,13 +1,6 @@
 class Family < ActiveRecord::Base
-
   has_many :users
-<<<<<<< HEAD
-
-=======
   has_many :events
->>>>>>> Add Family reference to Event
+
   validates :name, presence: true
-
-  has_many :events
-
 end
