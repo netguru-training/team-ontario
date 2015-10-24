46d27b19f6135b83d730a9a7b570815214b0bfe6
Magda Wiśniewska <magda@bardins.pl>
2015-10-24 17:18:57 +0200

    Add redirection to ParentPanel#index if User is Parent

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,9 @@
 class VisitorsController < ApplicationController
+  before_action :show_parent_panel
+
+  def show_parent_panel
+    if user_signed_in?
+      redirect_to parent_panel_index_path if current_user.has_role?(:parent)
+    end
+  end
 end
