4656c0592ab458dce3882603a32665dc097ac0ff
Grzegorz Bugala <drunkwolly@gmail.com>
2015-10-24 13:42:44 +0200

    Modify abilites

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,9 +3,22 @@ class Ability
 
   def initialize(user)
     if user.has_role? :parent
-      can :manage, :all
+      can :manage, Family do |family|
+        family.try(:user) == user
+      end
+      can :manage, Event do |event|
+        user.family == event.family
+      end
+      can :manage, Comment do |comment|
+        comment.try(:user) == user
+      end
+
+
     else
       can :read, :all
-end
-  end
+      can :craete, Comment
+      can :update, Comment do |comment|
+        comment.try(:user) == user || user.role?(:parent)
+      end
+    end
 end
