d820dda4ca1ae830a4e16656c52a02d915d7a34b
Juliusz Nadberezny <nadberezny@gmail.com>
2015-10-25 09:53:27 +0100

    Add create action in Comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
+  expose(:kid_event)
   expose(:comment)
   expose(:comments)
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: :create
 
   def index
   end
@@ -11,11 +12,17 @@ class CommentsController < ApplicationController
   end
 
   def create
+    self.comment = Comment.new(comment_params.merge(user_id: current_user.id))
+    if comment.save
+      redirect_to kid_event_comments_path(kid_event), notice: 'Event was succesfully created'
+    else
+      render :new, warning: event.errors.full_messages.to_sentence
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :user_id, :kid_event_id)
+    params.require(:comment).permit(:content, :kid_event_id)
   end
 end
